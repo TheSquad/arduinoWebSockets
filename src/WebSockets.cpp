3e36f6baee5c068f508d45bd34fbcb64a1837bca
Morgan Segalis <msegalis@gmail.com>
2025-11-27 11:51:37 +0100

    Replace DEBUG_WEBSOCKETS with ESP_LOGI and ESP_LOGE for improved logging in WebSockets handling

--- src/WebSockets.cpp
+++ src/WebSockets.cpp
@@ -431,12 +431,12 @@ void WebSockets::handleWebsocketCb(WSclient_t * client) {
         buffer += 8;
     }
 
-    DEBUG_WEBSOCKETS("[WS][%d][handleWebsocket] ------- read massage frame -------\n", client->num);
-    DEBUG_WEBSOCKETS("[WS][%d][handleWebsocket] fin: %u rsv1: %u rsv2: %u rsv3 %u  opCode: %u\n", client->num, header->fin, header->rsv1, header->rsv2, header->rsv3, header->opCode);
-    DEBUG_WEBSOCKETS("[WS][%d][handleWebsocket] mask: %u payloadLen: %u\n", client->num, header->mask, header->payloadLen);
+    ESP_LOGI("WS_CORE", "[WS][%d][handleWebsocket] ------- read massage frame -------\n", client->num);
+    ESP_LOGI("WS_CORE", "[WS][%d][handleWebsocket] fin: %u rsv1: %u rsv2: %u rsv3 %u  opCode: %u\n", client->num, header->fin, header->rsv1, header->rsv2, header->rsv3, header->opCode);
+    ESP_LOGI("WS_CORE", "[WS][%d][handleWebsocket] mask: %u payloadLen: %u\n", client->num, header->mask, header->payloadLen);
 
     if(header->payloadLen > WEBSOCKETS_MAX_DATA_SIZE) {
-        DEBUG_WEBSOCKETS("[WS][%d][handleWebsocket] payload too big! (%u)\n", client->num, header->payloadLen);
+        ESP_LOGE("WS_CORE", "[WS][%d][handleWebsocket] payload too big! (%u)\n", client->num, header->payloadLen);
         clientDisconnect(client, 1009);
         return;
     }
